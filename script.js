9b2d7e81f3fbd47d7c74eb042c45cd54a3552911
dhuddell <dan.huddell@gmail.com>
2015-10-12 22:12:22 -0400

    Commit 3- JQuery, function
    Jquery responsive to click
    Wincounter update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+'use strict';
 
-//
 var gameBoard = {
   'a': null,
   'b': null,
@@ -12,19 +12,65 @@ var gameBoard = {
   'i': null
 }
 
+// Initialize class losses .text
+var loss = 0;
+$('.losses').val(loss);
+$('.losses').text($('.losses').val());
+
+
+// Initialize class wins .text
+var win = 0;
+$(".wins").val(win);
+$('.wins').text($('.wins').val());
+
+//    JQUERY    JQUERY    JQUERY    JQUERY
+$(document).ready(function(){
+  //  SQUARE clickhandler
+  $('.square').on('click', function(){
+    gameBoard[this.id] = 'x';
+    $(this).css('background-color', '#000000');
+  });
+    //Change DOM to contain X
+  $('.square').on('click', function(){
+    getWinner();
+  });
+
+});
+
+////////////////////////////////////////////////////////
+
+
 ////////////////////////////////////////////////////////
 function getWinner() {
+
   if (winnerIs('x')) {
+    var wins = $(".wins").val();
+    var total = wins+1;
+    $('.wins').text(total);
+    alert('You have won!');
     return 'x';
   }
   if (winnerIs('o')) {
+    var losses = $(".losses").val();
+    var total = losses+1;
+    $('.losses').text(total);
+    alert('You have lost!');
     return 'o';
   }
+  alert('Tie, you idiot!');
   return null;
 }
 ////////////////////////////////////////////////////////
 
+////////////////////////////////////////////////////////
+//get opponent move
+var opponentMove = function(){
+  //Waits for update from AJAX on div (lines up to gameBoard(location))
+    //this changes value of location to opponent
+}
 
+
+////////////////////////////////////////////////////////
  //This will increment the win/lose counters on the DOM
   // It will take 'winner' from the output of getWinner
 var winCount = 0;
@@ -48,20 +94,20 @@ function winnerIs(player) {
 
 ////////////////////////////////////////////////////////
 function winsRow(player) {
-  return allThree(player, cells('a'), cells('b'), cells('c')) ||
-         allThree(player, cells('d'), cells('e'), cells('f')) ||
-         allThree(player, cells('g'), cells('h'), cells('i'));
+  return allThree(player, gameBoard['a'], gameBoard['b'], gameBoard['c']) ||
+         allThree(player, gameBoard['d'], gameBoard['e'], gameBoard['f']) ||
+         allThree(player, gameBoard['g'], gameBoard['h'], gameBoard['i']);
 }
 
 function winsColumn(player) {
-  return allThree(player, cells('a'), cells('d'), cells('g')) ||
-         allThree(player, cells('b'), cells('e'), cells('h')) ||
-         allThree(player, cells('c'), cells('f'), cells('i'));
+  return allThree(player, gameBoard['a'], gameBoard['d'], gameBoard['g']) ||
+         allThree(player, gameBoard['b'], gameBoard['e'], gameBoard['h']) ||
+         allThree(player, gameBoard['c'], gameBoard['f'], gameBoard['i']);
 }
 
 function winsDiagonal(player) {
-  return allThree(player, cells('a'), cells('e'), cells('i')) ||
-         allThree(player, cells('c'), cells('e'), cells('g'));
+  return allThree(player, gameBoard['a'], gameBoard['e'], gameBoard['i']) ||
+         allThree(player, gameBoard['c'], gameBoard['e'], gameBoard['g']);
 }
 
 ////////////////////////////////////////////////////////
